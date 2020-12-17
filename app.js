13ae3cb659d5c75feb725082fd6e8b2f1eb1abb5
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2020-12-16 22:55:30 -0600

    Creación de clases

---
+++ app.js
@@ -0,0 +1,32 @@
+//Clase de productos
+class Producto{
+
+    //Propiedades
+
+
+    //Constructor
+    constructor(name, price, year){
+        this.name = name
+        this.price = price
+        this.year = year
+    }
+}
+
+
+//Clase de la lista UI para despliegue de información
+class UI{
+    //Metodo para agregar
+    addProduct(){
+
+    }
+
+    //Borrar producto
+    deleteProducto(){
+
+    }
+
+    //Mostrar alerta 
+    showMessage(){
+
+    }
+}
